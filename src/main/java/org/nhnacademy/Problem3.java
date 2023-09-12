5a17d75e345c866698567a0c4b9e9d77f3c2190c
masiljangajji <hjk172262@gmail.com>
2023-09-12 14:39:08 +0900

    feat: Problem3 풀이

---
+++ src/main/java/org/nhnacademy/Problem3.java
@@ -0,0 +1,19 @@
+package org.nhnacademy;
+
+import java.util.Scanner;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Problem3 {
+
+    private static final Logger logger = LoggerFactory.getLogger(Problem3.class);
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        logger.info("사용자 이름을 입력해 주세요");
+        String userName = sc.next();
+
+        logger.info("Hello, {}, 만나서 반가워요!", userName);
+    }
+}
