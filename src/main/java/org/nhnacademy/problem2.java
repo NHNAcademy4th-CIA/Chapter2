4b79e2faaeaa5c3967dfd53c121c6efa93acb5bf
LeeJungBum <jungbum05114@gmail.com>
2023-09-12 15:35:37 +0900

    fix : test코드를 위한 리턴타입 정의

--- src/main/java/org/nhnacademy/problem2.java
+++ src/main/java/org/nhnacademy/problem2.java
@@ -22,13 +22,14 @@ public class problem2 {
         logger.info("Your total roll is " + "{}", first + second);
     }
 
-    public static void number2() {
+    public static String number2() {
         logger.info("이름을 입력하세요. : ");
         String name = scanner.next();
         logger.info("Hello, {} nice to meet you!", name.toUpperCase());
+        return name;
     }
 
-    public static void number3() {
+    public static double number3() {
         logger.info("가지고 있는 쿼터는 얼마인가요? : ");
         int quarter = scanner.nextInt();
         logger.info("가지고 있는 다임은 얼마인가요? : ");
@@ -37,10 +38,12 @@ public class problem2 {
         int nickel = scanner.nextInt();
         logger.info("가지고 있는 페니는 얼마인가요? : ");
         int penny = scanner.nextInt();
-        logger.info("당신이 가지고 있는 달러는 ? {} 달러입니다", Money.toDollar(penny, nickel, dime, quarter));
+        double dollar = Money.toDollar(penny, nickel, dime, quarter);
+        logger.info("당신이 가지고 있는 달러는 ? {} 달러입니다",dollar);
+        return dollar;
     }
 
-    public static void number4() {
+    public static String number4() {
         logger.info("몇개의 계란을 가지고 있습니까? : ");
         int eggs = scanner.nextInt();
         int gross = eggs / 144;
@@ -48,9 +51,9 @@ public class problem2 {
         int dozen = eggs / 12;
         eggs %= 12;
         logger.info("Your number of eggs is {} gross, {} dozen, and {}", gross, dozen, eggs);
+        return gross + " "+ dozen+ " "+ eggs;
     }
-
-    public static void number5() {
+    public static String number5() {
         logger.info("Please enter your first name and last name, separated by a space. : ");
         String name = scanner.nextLine();
         int space = name.indexOf(" ");
@@ -59,9 +62,10 @@ public class problem2 {
         logger.info("Your first name is {}, which has {} characters", firstName, firstName.length());
         logger.info("Your first name is {}, which has {} characters", secondName, secondName.length());
         logger.info("Your initials are {}{}", firstName.charAt(0), secondName.charAt(0));
+        return firstName.charAt(0)+""+secondName.charAt(1);
     }
 
-    public static void number6() {
+    public static int number6() {
         File note = new File("src/main/resources/testdata.txt");
         try {
             BufferedReader br = new BufferedReader(new FileReader(note));
@@ -70,10 +74,13 @@ public class problem2 {
             int score2= Integer.parseInt(br.readLine());
             int score3= Integer.parseInt(br.readLine());
             logger.info("{}의 평균 성적 {}",name,(score1+score2+score3)/3);
+            return (score1+score2+score3)/3;
+
         }catch (IOException e)
         {
             logger.info("위치에 파일이 없습니다.");
         }
+        return 0;
     }
     private static int dice() {
         return (int) (Math.random() * 6) + 1;
